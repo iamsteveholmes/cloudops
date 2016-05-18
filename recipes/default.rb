b048eb89c982c8cb7a2fecb02d87b5d1f9a117a4
Steve Holmes <iamsteveholmes@gmail.com>
2016-05-18 11:24:10 -0700

    specification and recipe to run on both vagrant and ec2

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,3 +1,13 @@
-file '/web/index.html' do
-	content '<html><body><p>Automation for the People</p></html>'
+package 'apache2'
+
+service 'apache2' do
+  supports :status => true
+  action [:enable, :start]
+end
+
+file '/var/www/html/index.html' do
+	content '<html>
+  <body>
+    <p>Automation for the People</p>
+</html>'
 end
